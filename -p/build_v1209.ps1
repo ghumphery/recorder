aa44ad6f68ba35317f68993aa71213a39aa83cb4
humphery <ghumphery@gmail.com>
2026-07-07 13:56:33 +0800

    chore(gitignore): stop tracking -p/ scratch directory

--- -p/build_v1209.ps1
+++
@@ -1,8 +0,0 @@
-# v1.20.9 編譯腳本
-Set-Location 'C:\Users\humphery\coding\recoder\frontend'
-$env:CSC_LINK = 'C:\Certs\recorder_selfsign.pfx'
-$env:CSC_KEY_PASSWORD = 'RecorderSelfSign2026'
-$logPath = 'C:\Users\humphery\AppData\Local\Temp\electron_builder_v1210.log'
-$errPath = 'C:\Users\humphery\AppData\Local\Temp\electron_builder_v1210_err.log'
-& '.\node_modules\.bin\electron-builder.cmd' --win portable --config.directories.output=dist-electron-build5 2>&1 | Out-File -FilePath $logPath -Encoding utf8
-"END_OK" | Out-File -FilePath "$logPath.done" -Encoding utf8
